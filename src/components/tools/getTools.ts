056b6ded14b0a87a1f5ac5db97f0bdbba4fafde9
Gus Ramírez <g.ram.bt@hotmail.com>
2021-01-20 23:22:48 -0600

    add navbar, add tooltip to svg

--- src/components/tools/getTools.ts
+++ src/components/tools/getTools.ts
@@ -19,24 +19,24 @@ import webpackSvg from './html/webpack.html';
 import { pauseAnimation } from '.';
 
 const tools = [
-  angularSvg,
-  css3Svg,
-  githubSvg,
-  html5Svg,
-  javaSvg,
-  javascriptSvg,
-  mongodbSvg,
-  mysqlSvg,
-  nextjsSvg,
-  nodejsSvg,
-  postgresqlSvg,
-  pugSvg,
-  pythonSvg,
-  reactSvg,
-  rxjsSvg,
-  typescriptSvg,
-  vueSvg,
-  webpackSvg,
+  { title: 'angular', html: angularSvg },
+  { title: 'css3', html: css3Svg },
+  { title: 'github', html: githubSvg },
+  { title: 'html5', html: html5Svg },
+  { title: 'java', html: javaSvg },
+  { title: 'javascript', html: javascriptSvg },
+  { title: 'mongodb', html: mongodbSvg },
+  { title: 'mysql', html: mysqlSvg },
+  { title: 'nextjs', html: nextjsSvg },
+  { title: 'nodejs', html: nodejsSvg },
+  { title: 'postgresql', html: postgresqlSvg },
+  { title: 'pug', html: pugSvg },
+  { title: 'python', html: pythonSvg },
+  { title: 'react', html: reactSvg },
+  { title: 'rxjs', html: rxjsSvg },
+  { title: 'typescript', html: typescriptSvg },
+  { title: 'vue', html: vueSvg },
+  { title: 'webpack', html: webpackSvg },
 ];
 
 export const getTools = (container: HTMLDivElement) => {
@@ -46,7 +46,8 @@ export const getTools = (container: HTMLDivElement) => {
   tools.forEach((tool) => {
     const div = document.createElement('div');
     div.className = 'tool';
-    div.innerHTML = tool;
+    div.innerHTML = tool.html;
+    div.setAttribute('data-title', tool.title);
     fragment.appendChild(div);
   });
 
