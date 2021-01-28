3a594757284849112a4c17b701a70f7533cf9a56
Gus Ramírez <g.ram.bt@hotmail.com>
2021-01-27 20:10:33 -0600

    add extra style and color to section tools and experience

--- utils/mydata.ts
+++ utils/mydata.ts
@@ -28,7 +28,7 @@ const data = [
   {
     title: 'MyClients',
     abstract:
-      'Backend/Frontend, Relational-DB, Authentication, Authorization, DOM-manipulation, Pagination, Documentation, Swagger, Postman',
+      'Backend/Front end, Relational-DB, Authentication, Authorization, DOM-manipulation, Pagination, Documentation, Swagger, Postman',
     description:
       'I was able to focus my knowledge from many sources and get this app up and running. It all start with choosing the database (postgresql, mainly because the free host) and the integration with the psql & psql-promises library beyond any ORM. In this way, I built the backend inside a nodeJS server dividing the store implementation for the server. It was too much work but it allowed me to absorb the application of many concepts. The data was initially provided by webscrapping and organized in tables applying relational database concepts. In addition middlewares was implemented to handle the authorization and authentication of users.  \n The frontend choice was Angular because it is fun and provides all the tools that frameworks can offer. I created services for data handling, pipes to render information appropriately, use rxjs operators to transform data flow and used modules to reinforce application performance',
     techs: [
